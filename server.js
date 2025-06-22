d83c38ddb27a71200e7a1bf128b96b43dfee34d6
Scott Denby <34488903+Awestruck3@users.noreply.github.com>
2025-06-21 23:41:17 -0400

    Another fix attempt

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ projectData.initialize();
 
 app.use(express.static('public'));
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + './public/views');
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/views/index.html'));
